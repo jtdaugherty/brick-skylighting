71c37cad45ac0a942523e0876f13ac6354d0ed59
Jonathan Daugherty <cygnus@foobox.com>
2018-01-22 21:50:09 -0800

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for brick-skylighting
+
+## 0.1  -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ src/Brick/Widgets/Skylighting.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Brick.Widgets.Skylighting
+  ( codeBlock
+  , codeBlock'
+
+  -- * Attributes
+  , attrNameForTokenType
+  , highlightedCodeBlockAttr
+  )
+where
+
+import Data.Monoid ((<>))
+import qualified Data.Text as T
+import qualified Data.Map as M
+
+import Brick
+
+import qualified Skylighting as Sky
+import Skylighting (TokenType(..))
+
+codeBlock :: Sky.Syntax -> T.Text -> Widget n
+codeBlock = codeBlock' txt
+
+codeBlock' :: (T.Text -> Widget n) -> Sky.Syntax -> T.Text -> Widget n
+codeBlock' renderToken syntax tx =
+    let expandedTabs = T.replace "\t" (T.replicate 8 " ") tx
+        cfg = Sky.TokenizerConfig (M.fromList [(Sky.sName syntax, syntax)]) False
+        result = Sky.tokenize cfg syntax expandedTabs
+    in case result of
+        Left _ -> withDefAttr highlightedCodeBlockAttr $ txt expandedTabs
+        Right tokLines -> rawCodeBlock renderToken tokLines
+
+rawCodeBlock :: (T.Text -> Widget n) -> [Sky.SourceLine] -> Widget n
+rawCodeBlock renderToken ls = vBox $ renderTokenLine renderToken <$> ls
+
+renderTokenLine :: (T.Text -> Widget n) -> Sky.SourceLine -> Widget n
+renderTokenLine _ [] = str " "
+renderTokenLine renderToken toks =
+    let renderSingle (ty, tx) = withDefAttr (attrNameForTokenType ty) $ renderToken tx
+    in hBox $ renderSingle <$> toks
+
+highlightedCodeBlockAttr :: AttrName
+highlightedCodeBlockAttr = "highlightedCodeBlock"
+
+attrNameForTokenType :: TokenType -> AttrName
+attrNameForTokenType ty = highlightedCodeBlockAttr <> attrName s
+    where
+        s = case ty of
+          KeywordTok        -> "keyword"
+          DataTypeTok       -> "dataType"
+          DecValTok         -> "declaration"
+          BaseNTok          -> "baseN"
+          FloatTok          -> "float"
+          ConstantTok       -> "constant"
+          CharTok           -> "char"
+          SpecialCharTok    -> "specialChar"
+          StringTok         -> "string"
+          VerbatimStringTok -> "verbatimString"
+          SpecialStringTok  -> "specialString"
+          ImportTok         -> "import"
+          CommentTok        -> "comment"
+          DocumentationTok  -> "documentation"
+          AnnotationTok     -> "annotation"
+          CommentVarTok     -> "comment"
+          OtherTok          -> "other"
+          FunctionTok       -> "function"
+          VariableTok       -> "variable"
+          ControlFlowTok    -> "controlFlow"
+          OperatorTok       -> "operator"
+          BuiltInTok        -> "builtIn"
+          ExtensionTok      -> "extension"
+          PreprocessorTok   -> "preprocessor"
+          AttributeTok      -> "attribute"
+          RegionMarkerTok   -> "regionMarker"
+          InformationTok    -> "information"
+          WarningTok        -> "warning"
+          AlertTok          -> "alert"
+          ErrorTok          -> "error"
+          NormalTok         -> "normal"
